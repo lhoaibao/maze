a76169cc05a750b4a3ddd2cf52f71996d27fcc2c
lhoaibao <lehoaibao081999@gmail.com>
2018-10-14 17:35:04 +0700

    new

--- test.py
+++ test.py
@@ -2,41 +2,23 @@
 import sys
 import collections
 
-# make a list to store maze
-maze = []
-APosition = []
-CoinPosition = []
-SpecialCoinPosition = []
-target = []
-
-sys.stdin.readline()
-sys.stdin.readline()
-print("I AM BAO\n")
-sys.stdin.readline()
-sys.stdin.readline()
-print("OK\n")
-sys.stdin.readline()
-
 def getMaze():
-    maze.clear()
-    line = sys.stdin.readline()
-    maze.append(line)
+    maze = []
+    sys.stdin.readline()
     while True:
         line = sys.stdin.readline()
-        if line == maze[0]:
-            maze.append(line)
+        if line[0] != "#":
             break
         maze.append(line)
+    return maze
 
 
-def getPositionOfA():
-    APosition.clear()
+def getPositionOfA(maze):
     y = 0
     for line in maze:
         if "A" in line:
             x = line.index("A")
-            APosition.extend([x,y])
-            break
+            return [x,y]
         else:
             y+=1
 
@@ -47,23 +29,36 @@ def bfs(grid, start):
       path = queue.popleft()
       y, x = path[-1]
       if grid[y][x] == 'o':
+          path.remove(path[0])
+          return path
+      if grid[y][x] == '!':
+          path.remove(path[0])
           return path
       for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
-          if 0 <= x2 < len(maze[0]) and 0 <= y2 < len(maze) and grid[y2][x2] != '#' and (y2, x2) not in seen:
+          if grid[y2][x2] != '#' and (y2, x2) not in seen:
               queue.append(path + [[y2, x2]])
               seen.add((y2, x2))
-def move():
-    getMaze()
-    getPositionOfA()
-    move = bfs(maze,APosition)
-    if move[1] == APosition[1] +1:
-        print("MOVE DOWN\n")
-    if move[1] == APosition[1] -1:
-        print("MOVE UP\n")
-    if move[0] == APosition[0] +1:
-        print("MOVE RIGHT\n")
-    if move[0] == APosition[0] -1:
-        print("MOVE LEFT\n")
+
+def move(A, go):
+    if go[0] - A[0] == 1:
+        return "MOVE RIGHT\n\n"
+    elif go[0] - A[0] == -1:
+        return "MOVE LEFT\n\n"
+    elif go[1] - A[1] == 1:
+        return "MOVE DOWN\n\n"
+    elif go[1] - A[1] == -1:
+        return "MOVE UP\n\n"
 
 
-move()
+line = sys.stdin.readline()
+while line != "":
+    if "HELLO"  in line:
+        sys.stdout.write("I AM BAO\n\n")
+    if "YOU ARE A" in line:
+        sys.stdout.write("OK\n\n")
+    if "MAZE" in line:
+        nowMaze = getMaze()
+        nowPosition = getPositionOfA(nowMaze)
+        move = bfs(nowMaze,(nowPosition[0],nowPosition[1]))
+        sys.stdout.write(move(nowPosition,move[0]))
+    line = sys.stdin.readline()
